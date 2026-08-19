e5c2b1d5f56b03f102482c6e0c1e79aa96a2e21a
Desi <dimova@gmail.com>
2026-08-19 17:21:45 +0300

    for loops

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,15 @@
 
 fun main() {
-    
+    for (i in 1..10) {
+        println("Hello, World! $i")
+    }
+    for (i in 10 downTo 1) {
+        println("Goodbye, World! $i")
+    }
+    for (i in 0 until 10) {
+        println("Counting up: $i")
+    }
+    for (i in 10 downTo 1 step 2) {
+        println("Counting down by 2: $i")
+    }
 }

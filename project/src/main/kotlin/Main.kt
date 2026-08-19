624e6f8650fa2fd841b41814fddde64e1e14e15a
Desi <dimova@gmail.com>
2026-08-19 19:31:40 +0300

    is even

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ import kotlin.random.Random
 fun main() {
     for (i in 0..4) {
         val random = Random.nextInt()
-        val isEven = random.rem(2) == 1
+        val isEven = random.rem(2) == 0
         when(isEven) {
             true -> println("$random is even")
             false -> println("$random is odd")

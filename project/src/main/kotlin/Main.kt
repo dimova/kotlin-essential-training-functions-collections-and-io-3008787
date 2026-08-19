6e6c6d34720103c3d48a2ad4045a563a100f605f
Desi <dimova@gmail.com>
2026-08-19 12:21:54 +0300

    Immutable, mutable variables. Inference and comparison

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,11 @@
 
 fun main() {
+    var ourFirstVariable = true
 
+    println(ourFirstVariable)
+
+    ourFirstVariable = false
+    println(ourFirstVariable)
+    println(ourFirstVariable==true)
+    println(ourFirstVariable==false)
 }

b5d178cfb57561f034c5dfc79e50b3727367cbda
Desi <dimova@gmail.com>
2026-08-19 15:56:47 +0300

    if and when statements or expressions

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,5 +1,54 @@
 
 
+fun getMessage(input: Int): String {
+    return if (input > 0) {
+        "Positive"
+    } else if (input < 0) {
+        "Negative"
+    } else {
+        "Zero"
+    }
+}
+//make it a single expression function
+fun getMessageSingleExpression(input: Int): String = if (input > 0) {
+    "Positive"
+} else if (input < 0) {
+    "Negative"
+} else {
+    "Zero"
+}
+
+//write a when expression function that takes an integer and returns a string indicating whether the number is even or odd
+fun getEvenOddMessage(input: Int): String {
+    return when (input % 2) {
+        0 -> "Even"
+        1, -1 -> "Odd"
+        else -> "Unknown"
+    }
+}
+
+//write a when single expression function that takes an integer and returns a string indicating whether the number is even or odd
+fun getEvenOddMessageSingleExpression(input: Int): String = when (input % 2
+) {
+    0 -> "Even"
+    1, -1 -> "Odd"
+    else -> "Unknown"
+}
+
 fun main() {
-    
+    //if statement vs if expression
+    val number = 10
+    //if statement
+    if (number > 0) {
+        println("$number is positive")
+    } else {
+        println("$number is not positive")
+    }
+    //if expression
+    val result = if (number > 0) {
+        "$number is positive"
+    } else {
+        "$number is not positive"
+    }
+    println(result)
 }

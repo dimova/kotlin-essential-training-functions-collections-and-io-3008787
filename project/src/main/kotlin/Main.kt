e67cf7c9291d5b9c14d31edb640374b8d6b610e3
Desi <dimova@gmail.com>
2026-08-19 17:26:52 +0300

    when loop exercise

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,14 +1,26 @@
 // Refactor getOutput() to use a when expression
 fun getOutput(input: Any?): String = if (input is Number) {
     if (input !is Int) {
-        "Input was an Int"
-    } else {
         "Input was a non-Int Number"
+    } else {
+        "Input was an Int"
     }
 }
 else if (input is String) { "Input was a String with length ${input.length}" }
 else if (input == null) { "Input was null" }
 else { "Input didn't match target inputs" }
+
+//rewrite the getOutput function using a when expression
+fun getOutput2(input: Any?): String {
+    return when (input) {
+        null -> "Input was null"
+        is Int -> "Input was an Int"
+        is Number -> "Input was a non-Int Number"
+        is String -> "Input was a String with length ${input.length}"
+        else -> "Input didn't match target inputs"
+
+    }
+}
 
 fun main() {
     println(getOutput(null))
@@ -16,4 +28,9 @@ fun main() {
     println(getOutput(3.2))
     println(getOutput("Hello Kotlin"))
     println(getOutput('a'))
+    println(getOutput2(null))
+    println(getOutput2(4))
+    println(getOutput2(3.2))
+    println(getOutput2("Hello Kotlin"))
+    println(getOutput2('a'))
 }

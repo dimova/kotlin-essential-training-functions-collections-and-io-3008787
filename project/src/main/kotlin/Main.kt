5b268e8de5ea7715176983a2cd5b02425f37ab22
Desi <dimova@gmail.com>
2026-08-19 14:10:48 +0300

    functional types

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,17 @@
 
+var greetingFunction: (String) -> String = { name -> "Hello, $name!" }
+
+var greetingFunction2: ()->Unit = { println("Hello, World!") }
+
+var greetingFunction3: (String)->String = { "Hello, $it" }
+
 fun main() {
-    
+    val name = "World"
+    println(greetingFunction(name))
+    greetingFunction2()
+    greetingFunction2 = { println("Hello, Kotlin!") }
+    greetingFunction2.invoke()
+    println(greetingFunction3("Kotlin"))
+    println(greetingFunction3("World"))
+    println(greetingFunction3.invoke("Everyone"))
 }

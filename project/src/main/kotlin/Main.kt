a0c111e4c414740255c9bd599ab35b3d4c1e024e
Desi <dimova@gmail.com>
2026-08-19 16:03:36 +0300

    try catch

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,5 +1,17 @@
 
 
 fun main() {
-    
+    try {
+        val message = "The value is ${10/0}"
+    } catch (e: ArithmeticException) {
+        println("Caught an exception: ${e.message}")
+    }
+
+    //Write try/catch as an expression
+    val result = try {
+        "The value is ${10/0}"
+    } catch (e: ArithmeticException) {
+        "Caught an exception: ${e.message}"
+    }
+    println(result)
 }

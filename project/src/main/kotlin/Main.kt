2c27b50105d179340d872d11c3561dcea1dfa034
Desi <dimova@gmail.com>
2026-08-19 17:03:37 +0300

    while loops

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,14 @@
 
 fun main() {
+    var counter = 0
+    while(counter < 5) {
+        println("Counter: $counter")
+        counter++
+        if(counter == 2) break
+    }
 
+    do {
+        println("Counter in do-while: $counter")
+        counter++
+    } while(counter < 5)
 }

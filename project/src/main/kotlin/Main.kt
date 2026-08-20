d111e7489a255beb0cd70376a5535fcc1a8feb53
Desi <dimova@gmail.com>
2026-08-20 18:20:45 +0300

    lists

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,15 @@
 
 fun main() {
-    
+    val stringList: List<String> = listOf("Book", "Pen", "Notebook")
+    val listOfInts: List<Int> = listOf(1, 2, 3, 4, 5)
+    val doubledValues: List<Int> = listOfInts.map { it * 2 }
+    doubledValues.forEach { println(it) }
+    val doubledValues2: List<Int> = List(5) { it * 2 }
+    doubledValues2.forEach { println(it) }
+    val languages = mutableListOf(
+            "Kotlin", "Java", "Python")
+    languages.add("Vue")
+    languages[1] = "JavaScript"
+    languages.remove("Python")
+        //mutableListOf extends List, so you can use all the methods of List
 }

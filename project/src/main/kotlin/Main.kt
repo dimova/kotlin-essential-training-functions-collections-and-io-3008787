c61bcc804e56a6026ce0408ab1e0c574713f8069
Desi <dimova@gmail.com>
2026-08-20 19:06:15 +0300

    challenge

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -2,6 +2,11 @@
 fun main() {
     // Using the provided test scores, identify the 3 students
     // with the lowest test scores
-
-    
+    testScores
+        .entries
+        .sortedBy { it.value }
+        .take(3)
+        .forEach { (studentId, score) ->
+            println("$studentId: $score")
+        }
 }

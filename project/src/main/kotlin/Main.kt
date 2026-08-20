a3a4fcad1186ab0545f1c199c11ad339ca154f5d
Desi <dimova@gmail.com>
2026-08-20 19:02:52 +0300

    sequences

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,4 +1,15 @@
 
 fun main() {
-    
+    val languages = listOf("Kotlin", "Java", "Python", "C++", "JavaScript")
+        .filter { it.length > 4 }
+        .map{ it.uppercase() }
+    val languageSequence = languages.asSequence()
+        .filter { it.startsWith("J") }
+        .map { it.reversed() }
+    val languageSequence2 = sequenceOf("Kotlin", "Java", "Python", "C++", "JavaScript")
+        .filter { it.length > 4 }
+        .map { it.uppercase() }
+        .filter { it.startsWith("J") }
+        .map { it.reversed() }
+    println(languageSequence2.toList())
 }
